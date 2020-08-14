77003d335ca21f36680532b02d6092ff55818c7e
ADRIAN <lm_adrian_98@hotmail.com>
2020-08-13 22:53:23 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -31,6 +31,6 @@ let urlDB;
        urlDB = 'mongodb://localhost:27017/dova'
     }else{
         urlDB = process.env.MONGO_URL; 
-         urlDB ='mongodb+srv://admin:Macarena24@cluster0.sie6u.mongodb.net/dova';
+        // urlDB ='mongodb+srv://admin:Macarena24@cluster0.sie6u.mongodb.net/dova';
    }
     process.env.URLDB = urlDB;
