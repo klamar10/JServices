c1cdacfba0496321579253bd21c5fe103d1a36d8
ADRIAN <lm_adrian_98@hotmail.com>
2020-08-15 01:54:04 -0500

    RestServer - usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -22,7 +22,7 @@ app.get('/usuario',[verificaToken, verificaRol ],(req, res) => {
 });
 app.get('/usuario:id',[verificaToken, verificaRol ],(req, res) => {
     let id = req.params.id
-    Usuario.findOne({id: id})
+    Usuario.findOne({_id: id})
         .exec((err, usuarios) => {
             if (err) {
                 return res.status(400).json(err);
